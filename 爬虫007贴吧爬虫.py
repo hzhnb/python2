2a5c3eb5f205a3a003c3901eee4eec5235b30ac1
hzhnb <2322529134@qq.com>
2019-11-30 16:26:10 +0800

    这是网络爬虫一次修改

--- 爬虫007贴吧爬虫.py
+++ 爬虫007贴吧爬虫.py
@@ -1,16 +1,16 @@
 import requests
 class TiebaSolider:
     def __init__(self,tieba_name):
-        self.header = {"User-Agent":" Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
+        self.header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
         self.tieba_name = tieba_name
-        self.url_temp = "https://tieba.baidu.com/f?kw="+tieba_name+"&ie=utf-8&pn={}"
+        self.url_temp = "https://tieba.baidu.com?kw="+tieba_name+"&ie=utf-8&pn={}"
     def get_url_list(self):  # 构造url列表
         url_list = []
         for i in range(1000):
             url_list.append(self.url_temp.format(i * 50))
         return url_list
     def parse_url(self,url):
-        response = requests.get(url,)
+        response = requests.get(url,headers=self.header)
         return response.content.decode()
     def save_html(self,html_str,page_num):
         file_path = "{}--第{}页.html".format(self.tieba_name,page_num)
